c3c1416d6b2c5320ff9515938dea5405ee0db496
Justin Boswell <boswej@amazon.com>
2021-05-21 10:45:11 -0700

    Broke C code into separate files, moved common stuff to php_aws_crt.h (#25)
    * Broke C code into separate files, moved common stuff to php_aws_crt.h
    
    * clang-format
    
    * Use a unity build to make PHP do the right thing with our lib

--- ext/php_aws_crt.h
+++ ext/php_aws_crt.h
@@ -54,4 +54,102 @@ ZEND_EXTERN_MODULE_GLOBALS(awscrt)
         {#name, sizeof(#name) - 1, NULL, 0, 0, pass_by_ref, allow_null, 0},
 #endif
 
+#include "api.h"
+#include "awscrt_arginfo.h"
+
+/* Utility macros borrowed from common */
+#define GLUE(x, y) x y
+
+#define RETURN_ARG_COUNT(_1_, _2_, _3_, _4_, _5_, count, ...) count
+#define EXPAND_ARGS(args) RETURN_ARG_COUNT args
+#define COUNT_ARGS_MAX5(...) EXPAND_ARGS((__VA_ARGS__, 5, 4, 3, 2, 1, 0))
+
+#define OVERLOAD_MACRO2(name, count) name##count
+#define OVERLOAD_MACRO1(name, count) OVERLOAD_MACRO2(name, count)
+#define OVERLOAD_MACRO(name, count) OVERLOAD_MACRO1(name, count)
+
+#define CALL_OVERLOAD(name, ...) GLUE(OVERLOAD_MACRO(name, COUNT_ARGS_MAX5(__VA_ARGS__)), (__VA_ARGS__))
+
+#define VARIABLE_LENGTH_ARRAY(type, name, length) type *name = alloca(sizeof(type) * (length))
+
+/*
+ * PHP utility APIs for this extension
+ */
+/*
+ * Exception throwing mechanism, will never return
+ */
+#define aws_php_throw_exception(...) CALL_OVERLOAD(_AWS_PHP_THROW_EXCEPTION, __VA_ARGS__);
+#define _AWS_PHP_THROW_EXCEPTION5(format, ...) zend_error_noreturn(E_ERROR, format, __VA_ARGS__)
+#define _AWS_PHP_THROW_EXCEPTION4(format, ...) zend_error_noreturn(E_ERROR, format, __VA_ARGS__)
+#define _AWS_PHP_THROW_EXCEPTION3(format, ...) zend_error_noreturn(E_ERROR, format, __VA_ARGS__)
+#define _AWS_PHP_THROW_EXCEPTION2(format, ...) zend_error_noreturn(E_ERROR, format, __VA_ARGS__)
+#define _AWS_PHP_THROW_EXCEPTION1(format) zend_error_noreturn(E_ERROR, format)
+
+/**
+ * throws an exception resulting from argument parsing, notes the current function name in the exception
+ */
+#define aws_php_argparse_fail()                                                                                        \
+    do {                                                                                                               \
+        aws_php_throw_exception("Failed to parse arguments to %s", __func__);                                          \
+    } while (0)
+
+/**
+ * calls zend_parse_parameters() with the arguments and throws an exception if parsing fails
+ */
+#define aws_php_parse_parameters(type_spec, ...)                                                                       \
+    do {                                                                                                               \
+        if (zend_parse_parameters(ZEND_NUM_ARGS(), type_spec, __VA_ARGS__) == FAILURE) {                               \
+            aws_php_argparse_fail();                                                                                   \
+        }                                                                                                              \
+    } while (0)
+
+/**
+ * calls zend_parse_parameters_none() and throws an exception if parsing fails
+ */
+#define aws_php_parse_parameters_none()                                                                                \
+    do {                                                                                                               \
+        if (zend_parse_parameters_none() == FAILURE) {                                                                 \
+            aws_php_argparse_fail();                                                                                   \
+        }                                                                                                              \
+    } while (0)
+
+/* Thread queue functions for managing PHP's optional threading situation */
+typedef struct _aws_php_task {
+    void (*callback)(void *); /* task function */
+    void (*dtor)(void *);     /* deletes task_data, if non-null */
+    void *data;
+} aws_php_task;
+
+/* maximum number of queued callbacks to execute at once. Since this is to support single-threaded usage,
+ * this can be a fairly small number, as how many callbacks could we reasonably be stacking up?! */
+#define AWS_PHP_THREAD_QUEUE_MAX_DEPTH 32
+
+typedef struct _aws_php_thread_queue {
+    aws_crt_mutex *mutex;
+    aws_php_task queue[AWS_PHP_THREAD_QUEUE_MAX_DEPTH];
+    size_t write_slot;
+    void *thread_id;
+} aws_php_thread_queue;
+
+extern aws_php_thread_queue s_aws_php_main_thread_queue;
+bool aws_php_is_main_thread(void);
+
+void aws_php_thread_queue_init(aws_php_thread_queue *queue);
+void aws_php_thread_queue_clean_up(aws_php_thread_queue *queue);
+void aws_php_thread_queue_push(aws_php_thread_queue *queue, aws_php_task task);
+bool aws_php_thread_queue_drain(aws_php_thread_queue *queue);
+
+/* called from worker thread to wait for the main thread to execute any queued work in PHP */
+void aws_php_thread_queue_yield(aws_php_thread_queue *queue);
+
+/* called from PHP thread to wait on async queued jobs, one of which MUST complete the promise */
+void aws_php_thread_queue_wait(aws_php_thread_queue *queue, aws_crt_promise *promise);
+
+/**
+ * generic dispatch mechanism to call a callback provided as a zval with arguments
+ * that are converted to zvals based on the arg_types format string
+ * Uses the same format string as zend_parse_parameters
+ */
+zval aws_php_invoke_callback(zval *callback, const char *arg_types, ...);
+
 #endif /* PHP_AWS_CRT_H */
---
+++ ext/signing.c
@@ -0,0 +1,286 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include "php_aws_crt.h"
+
+PHP_FUNCTION(aws_crt_signing_config_aws_new) {
+    if (zend_parse_parameters_none() == FAILURE) {
+        RETURN_NULL();
+    }
+
+    aws_crt_signing_config_aws *signing_config = aws_crt_signing_config_aws_new();
+    RETURN_LONG((zend_ulong)signing_config);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_release) {
+    zend_ulong php_signing_config = 0;
+
+    aws_php_parse_parameters("l", &php_signing_config);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_release(signing_config);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_algorithm) {
+    zend_ulong php_signing_config = 0;
+    zend_ulong php_algorithm = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_config, &php_algorithm);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_algorithm algorithm = php_algorithm;
+    aws_crt_signing_config_aws_set_algorithm(signing_config, algorithm);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_signature_type) {
+    zend_ulong php_signing_config = 0;
+    zend_ulong php_signature_type = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_config, &php_signature_type);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signature_type signature_type = php_signature_type;
+    aws_crt_signing_config_aws_set_signature_type(signing_config, signature_type);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_credentials_provider) {
+    zend_ulong php_signing_config = 0;
+    zend_ulong php_credentials_provider = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_config, &php_credentials_provider);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_credentials_provider *credentials_provider = (void *)php_credentials_provider;
+    aws_crt_signing_config_aws_set_credentials_provider(signing_config, credentials_provider);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_region) {
+    zend_ulong php_signing_config = 0;
+    const char *region = NULL;
+    size_t region_len = 0;
+
+    aws_php_parse_parameters("ls", &php_signing_config, &region, &region_len);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_region(signing_config, (uint8_t *)region, region_len);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_service) {
+    zend_ulong php_signing_config = 0;
+    const char *service = NULL;
+    size_t service_len = 0;
+
+    aws_php_parse_parameters("ls", &php_signing_config, &service, &service_len);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_service(signing_config, (uint8_t *)service, service_len);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_use_double_uri_encode) {
+    zend_ulong php_signing_config = 0;
+    zend_bool php_use_double_uri_encode = 0;
+
+    aws_php_parse_parameters("lb", &php_signing_config, &php_use_double_uri_encode);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_use_double_uri_encode(signing_config, php_use_double_uri_encode);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_should_normalize_uri_path) {
+    zend_ulong php_signing_config = 0;
+    zend_bool php_should_normalize_uri_path = 0;
+
+    aws_php_parse_parameters("lb", &php_signing_config, &php_should_normalize_uri_path);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_should_normalize_uri_path(signing_config, php_should_normalize_uri_path);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_omit_session_token) {
+    zend_ulong php_signing_config = 0;
+    zend_bool php_omit_session_token = 0;
+
+    aws_php_parse_parameters("lb", &php_signing_config, &php_omit_session_token);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_omit_session_token(signing_config, php_omit_session_token);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_signed_body_value) {
+    zend_ulong php_signing_config = 0;
+    const char *signed_body_value = NULL;
+    size_t signed_body_value_len = 0;
+
+    aws_php_parse_parameters("ls", &php_signing_config, &signed_body_value, &signed_body_value_len);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_signed_body_value(
+        signing_config, (uint8_t *)signed_body_value, signed_body_value_len);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_signed_body_header_type) {
+    zend_ulong php_signing_config = 0;
+    zend_ulong php_signed_body_header_type = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_config, &php_signed_body_header_type);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signed_body_header_type signed_body_header_type = php_signed_body_header_type;
+    aws_crt_signing_config_aws_set_signed_body_header_type(signing_config, signed_body_header_type);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_expiration_in_seconds) {
+    zend_ulong php_signing_config = 0;
+    zend_ulong php_expiration_in_seconds = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_config, &php_expiration_in_seconds);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_expiration_in_seconds(signing_config, php_expiration_in_seconds);
+}
+
+PHP_FUNCTION(aws_crt_signing_config_aws_set_date) {
+    zend_ulong php_signing_config = 0;
+    zend_ulong php_timestamp = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_config, &php_timestamp);
+
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+    aws_crt_signing_config_aws_set_date(signing_config, php_timestamp);
+}
+
+PHP_FUNCTION(aws_crt_signable_new_from_http_request) {
+    zend_ulong php_http_message = 0;
+
+    aws_php_parse_parameters("l", &php_http_message);
+
+    const aws_crt_http_message *http_message = (void *)php_http_message;
+    aws_crt_signable *signable = aws_crt_signable_new_from_http_request(http_message);
+    RETURN_LONG((zend_ulong)signable);
+}
+
+PHP_FUNCTION(aws_crt_signable_new_from_chunk) {
+    zend_ulong php_input_stream = 0;
+    const char *previous_signature = NULL;
+    size_t previous_signature_len = 0;
+
+    aws_php_parse_parameters("ls", &php_input_stream, &previous_signature, &previous_signature_len);
+
+    aws_crt_input_stream *input_stream = (void *)php_input_stream;
+    aws_crt_signable *signable =
+        aws_crt_signable_new_from_chunk(input_stream, (uint8_t *)previous_signature, previous_signature_len);
+    RETURN_LONG((zend_ulong)signable);
+}
+
+PHP_FUNCTION(aws_crt_signable_new_from_canonical_request) {
+    const char *canonical_request = NULL;
+    size_t canonical_request_len = 0;
+
+    aws_crt_signable *signable =
+        aws_crt_signable_new_from_canonical_request((uint8_t *)canonical_request, canonical_request_len);
+    RETURN_LONG((zend_ulong)signable);
+}
+
+PHP_FUNCTION(aws_crt_signable_release) {
+    zend_ulong php_signable = 0;
+
+    aws_php_parse_parameters("l", &php_signable);
+
+    aws_crt_signable *signable = (void *)php_signable;
+    aws_crt_signable_release(signable);
+}
+
+PHP_FUNCTION(aws_crt_signing_result_release) {
+    zend_ulong php_signing_result = 0;
+
+    aws_php_parse_parameters("l", &php_signing_result);
+    aws_crt_signing_result *result = (void *)php_signing_result;
+    aws_crt_signing_result_release(result);
+}
+
+PHP_FUNCTION(aws_crt_signing_result_apply_to_http_request) {
+    zend_ulong php_signing_result = 0;
+    zend_ulong php_http_request = 0;
+
+    aws_php_parse_parameters("ll", &php_signing_result, &php_http_request);
+    aws_crt_signing_result *result = (void *)php_signing_result;
+    aws_crt_http_message *request = (void *)php_http_request;
+
+    if (aws_crt_signing_result_apply_to_http_request(result, request)) {
+        aws_php_throw_exception(
+            "Failed to apply signing result to HTTP request: %s", aws_crt_error_name(aws_crt_last_error()));
+    }
+}
+
+typedef struct _signing_state {
+    aws_crt_promise *promise;
+    zval *on_complete;
+    aws_crt_signing_result *signing_result;
+    int error_code;
+} signing_state;
+
+/* called on main thread to deliver result to php */
+static void s_sign_aws_complete(void *data) {
+    signing_state *state = data;
+    aws_crt_promise *promise = state->promise;
+    zval *on_complete = state->on_complete;
+    aws_php_invoke_callback(on_complete, "ll", (zend_ulong)state->signing_result, (zend_ulong)state->error_code);
+}
+
+/* called from signing process in aws_sign_request_aws */
+static void s_on_sign_request_aws_complete(aws_crt_signing_result *result, int error_code, void *user_data) {
+    signing_state *state = user_data;
+    aws_crt_promise *promise = state->promise;
+
+    state->signing_result = result;
+    state->error_code = error_code;
+
+    /*
+     * Must execute PHP callback before this function returns, or signing_result will be killed
+     * so the callback is queued back to the main thread and will have run when yield returns
+     */
+    aws_php_task complete_callback_task = {
+        .callback = s_sign_aws_complete,
+        .data = state,
+    };
+    aws_php_thread_queue_push(&s_aws_php_main_thread_queue, complete_callback_task);
+    aws_php_thread_queue_yield(&s_aws_php_main_thread_queue);
+
+    if (error_code) {
+        aws_crt_promise_fail(promise, error_code);
+    } else {
+        aws_crt_promise_complete(promise, result, NULL);
+    }
+}
+
+PHP_FUNCTION(aws_crt_sign_request_aws) {
+    zend_ulong php_signable = 0;
+    zend_ulong php_signing_config = 0;
+    zval *php_on_complete = 0;
+
+    aws_php_parse_parameters("llz", &php_signable, &php_signing_config, &php_on_complete);
+
+    aws_crt_signable *signable = (void *)php_signable;
+    aws_crt_signing_config_aws *signing_config = (void *)php_signing_config;
+
+    aws_crt_promise *promise = aws_crt_promise_new();
+    signing_state state = {
+        .promise = promise,
+        .on_complete = php_on_complete,
+    };
+    int ret = aws_crt_sign_request_aws(signable, signing_config, s_on_sign_request_aws_complete, &state);
+    if (ret != 0) {
+        int last_error = aws_crt_last_error();
+        aws_crt_promise_fail(promise, last_error);
+        aws_php_throw_exception(
+            "aws_crt_sign_request_aws: error starting signing process: %s", aws_crt_error_name(last_error));
+    }
+
+    aws_php_thread_queue_wait(&s_aws_php_main_thread_queue, promise);
+
+done:
+    aws_crt_promise_delete(promise);
+    RETURN_LONG(ret);
+}
