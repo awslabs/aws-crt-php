f7d72a5a561322a79dd9288f139a418056fa0ed0
Justin Boswell <boswej@amazon.com>
2021-06-11 17:29:08 -0700

    Fixed version check

--- ci-test.sh
+++ ci-test.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-PHP_VERSION=$(php --version | head -1 | cut -f 2 -d' ')
+PHP_VERSION=$(php --version | head -1 | cut -f 2 -d' ' | cut -f1 -d.)
+
+make test-extension
 
 if [[ $PHP_VERSION -ge 7 ]]; then
   make test-ffi
-  make test-extension
-else
-  make test-extension
 fi
