e1fbad8ffc95271c1de8725ac2c0772ee24a8b81
Justin Boswell <boswej@amazon.com>
2021-07-21 15:43:59 -0700

    Fixed should_sign_header functionality

--- ext/signing.c
+++ ext/signing.c
@@ -160,29 +160,31 @@ typedef struct _should_sign_header_data {
 static void should_sign_header_task(void *data) {
     should_sign_header_data *task = data;
     zval result = aws_php_invoke_callback(task->should_sign_header, "s", task->header_name);
-    task->result = ZVAL_TRUE(result);
+    task->result = (Z_TYPE(result) == IS_TRUE);
     zval_ptr_dtor(&result);
 }
 
 static bool aws_php_should_sign_header(const char *header_name, size_t header_length, void *user_data) {
-    zval *header_name = NULL;
-    ZVAL_STRINGL(php_header_name, header_name, header_length);
+    zval php_header_name;
+    ZVAL_STRINGL(&php_header_name, header_name, header_length);
 
     should_sign_header_data task_data = {
         .should_sign_header = user_data,
-        .header_name = header_name,
+        .header_name = &php_header_name,
+        .result = false,
     };
 
     aws_php_task task = {
         .callback = should_sign_header_task,
         .data = &task_data,
-        .result = false,
     };
 
-    aws_php_thread_queue_push(&s_aws_php_main_thread_queue, &task);
+    aws_php_thread_queue_push(&s_aws_php_main_thread_queue, task);
     aws_php_thread_queue_yield(&s_aws_php_main_thread_queue);
 
-    return task.result;
+    zval_ptr_dtor(&php_header_name);
+
+    return task_data.result;
 }
 
 PHP_FUNCTION(aws_crt_signing_config_aws_set_should_sign_header_fn) {
