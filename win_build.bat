2fc8357079a6cbf3dd29efb707e4a986a6f4a671
Dengke Tang <dengket@amazon.com>
2022-11-04 10:50:24 -0700

    vswhere

--- win_build.bat
+++ win_build.bat
@@ -3,6 +3,7 @@
 git clone https://github.com/Microsoft/php-sdk-binary-tools.git php-sdk
 cd ./php-sdk
 
-vswhere -nologo -version "[15,16)" -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath -format text
+vswhere
+@REM  -nologo -version "[15,16)" -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath -format text
 
 ./phpsdk-vc15-x64.bat
