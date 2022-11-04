4060e9831e38a92700f9b7f5fc089c52ac1aeabf
Dengke Tang <dengket@amazon.com>
2022-11-04 10:40:12 -0700

    what's that?

--- win_build.bat
+++ win_build.bat
@@ -2,4 +2,7 @@
 
 git clone https://github.com/Microsoft/php-sdk-binary-tools.git php-sdk
 cd ./php-sdk
+
+vswhere -nologo -version !PHP_SDK_VS_RANGE! -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath -format text
+
 ./phpsdk-vc15-x64.bat
