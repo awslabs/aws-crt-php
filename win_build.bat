d1d5004f11487253cb8c18af14a819af54cc26db
Dengke Tang <dengket@amazon.com>
2022-11-07 15:19:41 -0800

    silent the copy folder

--- win_build.bat
+++ win_build.bat
@@ -9,7 +9,7 @@ git clone https://github.com/php/php-src.git && cd php-src
 
 call phpsdk_deps --update --branch master
 
-if (%SOURCE_DIR%)==() (git clone --recursive https://github.com/awslabs/aws-crt-php.git ..\pecl\awscrt) else (robocopy %SOURCE_DIR% ..\pecl\awscrt /E)
+if (%SOURCE_DIR%)==() (git clone --recursive https://github.com/awslabs/aws-crt-php.git ..\pecl\awscrt) else (robocopy %SOURCE_DIR% ..\pecl\awscrt /E /NFL /NDL /NJH /NJS /nc /ns /np)
 
 call buildconf
 
