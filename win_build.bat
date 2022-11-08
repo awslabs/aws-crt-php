6cdba5bb0335bbab82eb8622736b619c0a5d974b
Dengke Tang <dengket@amazon.com>
2022-11-08 10:24:00 -0800

    what's the error for 8.0?

--- win_build.bat
+++ win_build.bat
@@ -3,11 +3,11 @@
 @REM Passing the source dirctory of aws-crt-php, if not passing the source directory, it will be pulled from github
 if not (%1)==() set SOURCE_DIR=%1
 
-call phpsdk_buildtree phpmaster
+call phpsdk_buildtree php-8.0
 
 git clone https://github.com/php/php-src.git && cd php-src
 
-call phpsdk_deps --update --branch master
+call phpsdk_deps --update --branch 8.0
 
 if (%SOURCE_DIR%)==() (git clone --recursive https://github.com/awslabs/aws-crt-php.git ..\pecl\awscrt) else (robocopy %SOURCE_DIR% ..\pecl\awscrt /E /NFL /NDL /NJH /NJS /nc /ns /np)
 
