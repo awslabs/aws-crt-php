26ab397cd5c16e1f2b6ed72b2af8cf9a35ab6949
Dengke Tang <dengket@amazon.com>
2023-02-23 14:54:30 -0800

    windows CI

--- dev-scripts/run_tests.bat
+++ dev-scripts/run_tests.bat
@@ -1,6 +1,18 @@
-
 @echo on
+setlocal
 
-cd ..
+REM Run command to get composer_dir
+set "composer_dir="
+for /f "usebackq delims=" %%i in (`where composer.phar`) do set "composer_dir=%%i"
 
-%PHP_BINARY% -c php-win.ini vendor/bin/phpunit tests --debug
+REM Check if composer_dir was found
+if "%composer_dir%"=="" (
+  echo No composer found.
+  exit /b 1
+) else (
+  cd ..
+  php -c php-win.ini %composer_dir% update
+  php -c php-win.ini vendor/bin/phpunit tests --debug
+)
+
+endlocal
