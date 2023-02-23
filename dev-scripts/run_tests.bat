311edc63700f08093929333ad2f9e661e0728003
Dengke Tang <dengket@amazon.com>
2023-02-23 15:17:19 -0800

    why?

--- dev-scripts/run_tests.bat
+++ dev-scripts/run_tests.bat
@@ -14,6 +14,7 @@ if "%composer_dir%"=="" (
   set "script_dir=%~dp0"
   set "work_dir=%script_dir%/.."
   cd %work_dir%
+  ls
 
   php -c php-win.ini %composer_dir% update
   php -c php-win.ini vendor/bin/phpunit tests --debug
