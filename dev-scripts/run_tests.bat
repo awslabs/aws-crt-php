8208b31f5d645e7aef45725d3d037d2eb5aa50e1
Dengke Tang <dengket@amazon.com>
2023-02-23 15:05:57 -0800

    get the right directory

--- dev-scripts/run_tests.bat
+++ dev-scripts/run_tests.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 setlocal
 
 REM Run command to get composer_dir
@@ -11,6 +11,10 @@ if "%composer_dir%"=="" (
   exit /b 1
 ) else (
   cd ..
+  set "script_dir=%~dp0"
+  set "work_dir=%script_dir%/.."
+  cd %work_dir%
+
   php -c php-win.ini %composer_dir% update
   php -c php-win.ini vendor/bin/phpunit tests --debug
 )
