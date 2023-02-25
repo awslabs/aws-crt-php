33f6c7aaf8270738d0837e670200875951997b00
Dengke Tang <dengket@amazon.com>
2023-02-25 00:21:35 -0800

    fix batch script and add gitignore for windowa

--- dev-scripts/run_tests.bat
+++ dev-scripts/run_tests.bat
@@ -13,20 +13,19 @@ set "work_dir=%script_dir%/.."
 cd %work_dir%
 
 set vendor_path=vendor
+REM Run command to get composer_dir
+set "composer_dir="
+for /f "usebackq delims=" %%i in (`where composer.phar`) do set "composer_dir=%%i"
 
 if not exist "%vendor_path%" (
-  REM Run command to get composer_dir
-  set "composer_dir="
-  for /f "usebackq delims=" %%i in (`where composer.phar`) do set "composer_dir=%%i"
-
   REM Check if composer_dir was found
   if "%composer_dir%"=="" (
     echo No composer found.
     exit /b 1
   )
+  call %PHP_BINARY% -c php-win.ini %composer_dir% update
 )
 
-call %PHP_BINARY% -c php-win.ini %composer_dir% update
 call %PHP_BINARY% -c php-win.ini vendor/bin/phpunit tests --debug
 
 endlocal
