f507c6c95c5a82c75e22f8655a29c085d921edf6
Dengke Tang <dengket@amazon.com>
2023-02-25 00:10:35 -0800

    fix stuff

--- dev-scripts/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# get the absolute path of the script file
-script_path = os.path.abspath(__file__)
-
-# get the directory of the script file
-script_dir = os.path.dirname(script_path)
-
-print("Directory of the script file:", script_dir)
