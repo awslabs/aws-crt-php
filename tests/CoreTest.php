a2f5d5731f64f4e748ec7fb57a661ad802c5e47a
Dengke Tang <dengket@amazon.com>
2023-03-01 16:49:55 -0800

    support cross version phpunit for supporting php8.1 and 8.2

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -4,19 +4,23 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 use AWS\CRT\CRT;
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
 
 require_once('common.inc');
 
 // This intentionally does not inherit from CrtTestCase because it needs a clean-room environment
-final class CoreTest extends PHPUnit_Framework_TestCase {
+final class CoreTest extends TestCase
+{
 
     // The CRT should always be available in this test suite
-    public function testIsAvailable() {
+    public function testIsAvailable()
+    {
         $this->assertTrue(CRT::isAvailable());
     }
 
     // We have done nothing to necessitate loading the CRT, it should not be loaded
-    public function testIsLoaded() {
+    public function testIsLoaded()
+    {
         $this->assertTrue(!CRT::isLoaded());
     }
 }
--- tests/ErrorTest.php
+++ tests/ErrorTest.php
@@ -4,18 +4,23 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 use AWS\CRT\CRT;
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
 
-final class ErrorTest extends PHPUnit_Framework_TestCase {
+final class ErrorTest extends TestCase
+{
 
-    public function testNoInitialError() {
+    public function testNoInitialError()
+    {
         $this->assertEquals(0, CRT::last_error());
     }
 
-    public function testCanResolveErrorName() {
+    public function testCanResolveErrorName()
+    {
         $this->assertEquals("AWS_ERROR_SUCCESS", CRT::error_name(0));
     }
 
-    public function testCanResolveErrorStr() {
+    public function testCanResolveErrorStr()
+    {
         $this->assertEquals("Success.", CRT::error_str(0));
     }
 }
